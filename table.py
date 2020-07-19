96b6d2bf56018862cb0b08f8d437c9b9585a5ec6
BIP <55277495+satya0150@users.noreply.github.com>
2020-07-19 16:35:36 +0530

    initial commit
    we get table in range (1-20)

---
+++ table.py
@@ -0,0 +1,65 @@
+
+data = input("which table you want to see in range (2 - 20):\t")
+
+if data == "1":
+    for x in range(1,11):
+        print(x)
+if data == "2":
+    for x in range(2,21,2):
+        print(x)
+if data == "3":
+    for x in range(3,31,3):
+        print(x)
+if data == "4":
+    for x in range(4,41,4):
+        print(x)
+if data == "5":
+    for x in range(5,51,5):
+        print(x)
+if data == "6":
+    for x in range(6,61,6):
+        print(x)
+if data == "7":
+    for x in range(7,71,7):
+        print(x)
+if data == "8":
+    for x in range(8,81,8):
+        print(x)
+if data == "9":
+    for x in range(9,91,9):
+        print(x)
+if data == "10":
+    for x in range(10,101,10):
+        print(x)
+if data == "11":
+    for x in range(11,111,11):
+        print(x)
+if data == "12":
+    for x in range(12,121,12):
+        print(x)
+if data == "13":
+    for x in range(13,131,13):
+        print(x)
+if data == "14":
+    for x in range(14,141,14):
+        print(x)
+if data == "15":
+    for x in range(15,151,15):
+        print(x)
+if data == "16":
+    for x in range(16,161,16):
+        print(x)
+if data == "17":
+    for x in range(17,171,17):
+        print(x)
+if data == "18":
+    for x in range(18,181,18):
+        print(x)
+if data == "19":
+    for x in range(19,191,19):
+        print(x)
+if data == "20":
+    for x in range(20,201,20):
+        print(x)
+else:
+    print("please put no. in range 20")
